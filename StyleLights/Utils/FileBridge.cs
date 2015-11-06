0938add9f622b7c400354fc9dec37ce1ce5b6072
Luke Miller <millerlj@rose-hulman.edu>
2015-11-05 21:43:28 -0500

    Added FileBridge, removed coreProj

---
+++ StyleLights/Utils/FileBridge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections;
+using System.IO;
+
+namespace StyleLightsCore
+{
+	public class Utils
+	{
+		//store patterns
+		private Hashtable patterns;
+		const string PatternFileName = "patterns";
+		//constructor reads file and closes handles
+		public Utils(){
+			patterns = new Hashtable ();
+			var inputString="";
+			using (var input = new FileStream (PatternFileName, System.IO.FileMode.OpenOrCreate)) {
+				inputString = input.ToString();
+			}
+				
+		}
+
+		public string getPattern(string PatternName) {
+			return (string) patterns[PatternName];//may return null
+		}
+		public void savePattern(string patternName, string PatternBlob){
+			patterns [patternName]=PatternBlob;
+		}
+	}
+}
+
+
